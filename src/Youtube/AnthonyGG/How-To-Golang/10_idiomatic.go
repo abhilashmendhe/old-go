d6a0b252295c73682a6753de7bbb91313f57065e
Abhilash <abhilash@localhost.localdomain>
2022-12-09 14:50:28 +0100

    addedfile go idiomatic

--- src/Youtube/AnthonyGG/How-To-Golang/10_idiomatic.go
+++ src/Youtube/AnthonyGG/How-To-Golang/10_idiomatic.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"sync"
+)
 
-// for constant
+// 1) for constant
 const scalar = 0.1 // if you want to export then you just capatilize the first letter of the variable name
 
-// variable grouping
+// 2) variable grouping
 func Foo() int {
 
 	// variable grouping
@@ -23,13 +27,87 @@ func Foo() int {
 	return x + y
 }
 
-// functions that panic
-func parseIntFromString(s string) (int, error) {
+// 3) functions that panic, if there is a panic then always prefix with "Must" to your function name
+func MustParseIntFromString(s string) (int, error) {
 	// Logic
-
+	panic("oops")
 	return 10, nil
 }
 
+// 4) how to struct initialize
+type Vector struct {
+	x int
+	y int
+}
+
+// 5) Mutex grouping
+type Server struct {
+	listenAddress string
+	isRunning     bool
+
+	// here the "map" type we want to protect therefore we always put mutex above the type that we want to protect...
+	// for mutex the variable name should alwyas contain a suffix ("mu" or "Lock")
+	mu    sync.RWMutex
+	peers map[string]net.Conn
+}
+
+// 6) interface declarations/naming, you should add a suffix "er"
+type Storager interface {
+	Get()
+	Put()
+	Delete()
+	Patch()
+}
+
+type Getter interface {
+	Get()
+}
+
+type Putter interface {
+	Put()
+}
+
+// 7) function grouping, so always put your exported functino at the top of your file and then all private function
+// to be placed below
+
+func VeryImportantFuncExported() {} // exported
+func veryImportantFunc()         {} // unexported
+
+// 8) http handler naming, "handle" with prefix
+func handleGetUserById() {}
+func handleResizeImage() {}
+
+// 9) enum
+
+type Suit byte
+
+// you should add the prefix type name to all the const variable, like below you can see Suit(...), Suit is type..
+const (
+	SuitHearts Suit = iota
+	SuitClubs
+	SuitDiamonds
+	SuitSpades
+)
+
+// 10 constructor
+
+// we want to construct this Order
+type Order struct {
+	Size float64
+}
+
+func (o Order) NewOrder(size float64) *Order {
+	// logic here
+	return &Order{Size: size}
+}
 func main() {
 
+	//4) this is how you initialize with name variables....
+	vec := Vector{
+		x: 10,
+		y: 20,
+	}
+	fmt.Println(vec)
+	oo := &Order{Size: 10.2}
+	oo.NewOrder(100)
 }
